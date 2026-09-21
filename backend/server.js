d15dc29d27c47e0b4ebaef456fe225f87bec2329
Aadi0118 <sinhaaditya0110@gmail.com>
2026-09-21 15:24:36 +0530

    fix: remove deprecated mongoose connection options

--- backend/server.js
+++ backend/server.js
@@ -259,7 +259,7 @@ io.on('connection', (socket) => {
 const PORT = process.env.PORT || 5000;
 
 if (process.env.MONGO_URI) {
-  mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  mongoose.connect(process.env.MONGO_URI)
     .then(() => {
       console.log('Connected to MongoDB');
       server.listen(PORT, () => {
